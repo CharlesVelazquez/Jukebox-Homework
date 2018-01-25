5315ea9123d8754937cf0b3a8431c8227b47328d
Charles Velazquez <CharlesVelazquez15@gmail.com>
2018-01-25 15:27:03 -0500

    Finally starting to get it, starting audio stuff

---
+++ index.js
@@ -0,0 +1,34 @@
+var jukebox = {
+	name: 'jukebox',
+	play: function (argument) {
+			// body...
+			},
+	reverse: function (argument) {
+			// body...
+			},
+	fastfoward: function (argument) {
+			// body...
+			},
+	shuffle: function (argument) {
+			// body...
+			},
+}
+
+var songs = {
+	song1: {
+		title: "test",
+		src: "FAIRYTAILMainThemePiano.mp3"
+	},
+	song2: {
+		title: {},
+		src: "FairyTailSlowversionMainTheme.mp3"
+	},
+	song3: {
+		title: {},
+		src: "OtoutoMitainaSonzaiPianoSolo.mp3"
+	},
+	song4: {
+		title: {},
+		src: "YuujinAkunwoWatashinoBansoushaniNinmeishimasu.mp3"
+	}
+}
